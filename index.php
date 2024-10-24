b175c553d9c2ec0bf7dc27b80b71863a256f57f3
Gerito <you@example.com>
2024-10-23 20:14:05 -0500

    index.php realizado

--- index.php
+++ index.php
@@ -4,13 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyecto Final</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
+    <header>
 
-<h1>
-    Dashboard
-</h1>
-    
+        <nav>
+            <a href="index.php">Inicio</a>
+            <a href="Login.php">Contacto</a>
+            <a href="dashboard.php">Blog</a>
+        
+
+        </nav>
+    </header>
+    <main>
+        <h1>Bienevenidos a nuestro sistema CRUD</h1>
+        <p>
+            Este sistema permite la gestion de usuarios, un brud basico y conexion a base de datos
+        </p>
+    </main>
+
 </body>
 </html>
