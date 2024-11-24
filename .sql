640e2b5d54e74a5c0666956223d2d215c51d9002
Gerito <you@example.com>
2024-11-23 21:25:20 -0500

    toca arreglar el carrito

--- .sql
+++ .sql
@@ -6,17 +6,15 @@ CREATE TABLE usuarios(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE productos(
-
-		id Primaria	int(11)		AUTO_INCREMENT	PRIMARY KEY,				
-		nombre	varchar(100)		No	NULL,		
-		descripcion	text			No	NULL,			
-		precio	decimal(10,2)		No	NULL,			
-		stock	int(11)			    No	NULL,		
-		imagen	varchar(255)			NULL		
-
-
+CREATE TABLE productos (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion TEXT NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    stock INT(11) NOT NULL,
+    imagen VARCHAR(255) DEFAULT NULL
 );
+
 CREATE TABLE carrito (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT,
